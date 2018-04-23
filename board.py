cb8097b25e039045235b5686d2173bd28c98b3e3
Qiang Qu <qiangqu@Qiangs-MBP.gateway>
2018-04-24 00:47:19 +1000

    modify-model-input

--- board.py
+++ board.py
@@ -48,6 +48,8 @@ class Board:
                             {(0, BOARD_WIDTH-1): 0, (1, BOARD_WIDTH-1): 1, (0, BOARD_WIDTH-2): 2,
                              (2, BOARD_WIDTH-1): 3, (1, BOARD_WIDTH-2): 4, (0, BOARD_WIDTH-3): 5}]
 
+        self.hist_moves = []
+
 
     def check_win(self):
         """
@@ -196,7 +198,10 @@ class Board:
 
         # Update history
         self.board = np.concatenate((np.expand_dims(cur_board, axis=2), self.board[:, :, :NUM_HIST_MOVES - 1]), axis=2)
-
+        if len(self.hist_moves) == (NUM_HIST_MOVES-1)*2:
+            self.hist_moves = self.hist_moves [2:]
+        self.hist_moves.append(origin_pos)
+        self.hist_moves.append(dest_pos)
         return self.check_win()
 
     def player_progress(self, player_id):
